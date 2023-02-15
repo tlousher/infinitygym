0eac26bb1576c46806acec1c69a494451c1f95c0
Harold Troya <tlousher@gmail.com>
2023-02-15 18:26:45 -0500

    Landing | work in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './icons/logo.svg';
 import './App.css';
 
 function App() {
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is a test <code>FOR INFINITY</code>.
+          This webpage is training to become something great.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
