e90dc66dac019252d94b15768f722574ae3f7306
Harold Troya <tlousher@gmail.com>
2023-03-27 19:00:43 -0500

    Training | Color fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This webpage is training to become something great.
+          Training to become something great.
         </p>
       </header>
     </div>
