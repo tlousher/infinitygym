58dfc4be0db53aa9ccd01929cc32ca41e27e27a0
Harold Troya <tlousher@gmail.com>
2023-03-28 10:38:01 -0500

    Tailwind install

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import {Card, Typography} from "antd";
 
+
 const { Title } = Typography;
 const Home = () => {
     return (
-        <Card
-            hoverable
-            style={{ backgroundImage: `url('https://source.unsplash.com/random/800x400')`, backgroundSize: 'cover' }}
-        >
-            <Title style={{ color: 'white', textAlign: 'center' }}>Inspiring text here</Title>
-        </Card>
+        <div>
+            <Card
+                className={'h-screen bg-cover bg-center'}
+                style={{
+                    backgroundImage: `url(${process.env.PUBLIC_URL}/resources/images/weights.jpg)`
+                }}
+            >
+                <Title style={{ color: 'white', textAlign: 'center' }}>Inspiring text here</Title>
+            </Card>
+            <Card>
+                <Title>Home</Title>
+            </Card>
+        </div>
     );
 };
 
