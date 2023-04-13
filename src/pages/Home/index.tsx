f85f538270c8296cf42c1448860d1b27b76e479d
Harold Troya <tlousher@gmail.com>
2023-04-13 08:24:25 -0500

    Kids Banner

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -53,20 +53,33 @@ const DescriptiveIcon = ({icon, description, title}: {icon: React.ReactNode, des
     )
 }
 
-const TrainingImage = ({icon, title, description}: {icon: React.ReactNode, title: React.ReactNode, description: React.ReactNode}) => {
+const HoveringImage = ({icon, title, description, image}: {
+    description: React.ReactNode,
+    icon: React.ReactNode,
+    image: string,
+    title: React.ReactNode
+}) => {
     return (
-        <div className={styles.imageFooter}>
-            <div className={`${styles.footerCircle} flex justify-center items-center`}>
-                {icon}
-            </div>
-            <div className={styles.footerText}>
-                {title}
-            </div>
-            <div className={styles.footerDescription}>
-                <div className={'font-bold text-base'}>{title}</div>
-                {description}
-            </div>
-        </div>
+        <Col className={styles.imageContainer} xs={24} md={12} lg={8}>
+            <Image
+                src={`${process.env.PUBLIC_URL}/resources/images/${image}.jpg`}
+                style={{maxWidth: 250, maxHeight:250}}
+                footer={
+                    <div className={styles.imageFooter}>
+                        <div className={`${styles.footerCircle} flex justify-center items-center`}>
+                            {icon}
+                        </div>
+                        <div className={styles.footerText}>
+                            {title}
+                        </div>
+                        <div className={styles.footerDescription}>
+                            <div className={'font-bold text-base'}>{title}</div>
+                            {description}
+                        </div>
+                    </div>
+                }
+            />
+        </Col>
     )
 }
 
@@ -108,72 +121,19 @@ const Home = () => {
                             title={'Busca tu camino'}
                             description={'Tenemos varios estilos de entrenamiento para que encuentres el que más se adapte a ti'}
                         />
-                        <Col className={styles.imageContainer} xs={24} md={12} lg={8}>
-                            <Image
-                                src={`${process.env.PUBLIC_URL}/resources/images/jujitsu_ai.jpg`}
-                                style={{maxWidth: 250, maxHeight:250}}
-                                footer={
-                                    <TrainingImage
-                                        icon={<BoxingGlovesIcon style={{fontSize: 30}} />}
-                                        title={'Jujitsu'}
-                                        description={'Arte marcial de defensa personal y sumisión.'}
-                                    />
-                                }
-                            />
-                        </Col>
-                        <Col className={styles.imageContainer} xs={24} md={12} lg={8}>
-                            <Image
-                                src={`${process.env.PUBLIC_URL}/resources/images/kickboxing_ai.jpg`}
-                                style={{maxWidth: 250, maxHeight:250}}
-                                footer={
-                                    <TrainingImage
-                                        icon={<BoxingGlovesIcon style={{fontSize: 30}} />}
-                                        title={'Kickboxing'}
-                                        description={'Deporte de combate con golpes y patadas.'}
-                                    />
-                                }
-                            />
-                        </Col>
-                        <Col className={styles.imageContainer} xs={24} md={12} lg={8}>
-                            <Image
-                                src={`${process.env.PUBLIC_URL}/resources/images/muaythai_ai.jpg`}
-                                style={{maxWidth: 250, maxHeight:250}}
-                                footer={
-                                    <TrainingImage
-                                        icon={<BoxingGlovesIcon style={{fontSize: 30}} />}
-                                        title={'Muay Thai'}
-                                        description={'Arte marcial tailandés de ocho extremidades.'}
-                                    />
-                                }
-                            />
-                        </Col>
-                        <Col className={styles.imageContainer} xs={24} md={12} lg={8}>
-                            <Image
-                                src={`${process.env.PUBLIC_URL}/resources/images/olimpic_ai.jpg`}
-                                style={{maxWidth: 250, maxHeight:250}}
-                                footer={
-                                    <TrainingImage
-                                        icon={<BoxingGlovesIcon style={{fontSize: 30}} />}
-                                        title={'Lucha olímpica'}
-                                        description={'Lucha cuerpo a cuerpo en busca de dominación.'}
-                                    />
-                                }
-                            />
-                        </Col>
-                        <Col className={styles.imageContainer} xs={24} md={12} lg={8}>
-                            <Image
-                                src={`${process.env.PUBLIC_URL}/resources/images/mma_ai.jpg`}
-                                style={{maxWidth: 250, maxHeight:250}}
-                                footer={
-                                    <TrainingImage
-                                        icon={<BoxingGlovesIcon style={{fontSize: 30}} />}
-                                        title={'MMA'}
-                                        description={'Deporte de combate mixto con múltiples disciplinas.'}
-                                    />
-                                }
-                            />
-                        </Col>
-                    {/*    Jujitsu, kickboxing, Muay Thai, lucha olímpica, MMA */}
+                        <HoveringImage description={'Arte marcial de defensa personal y sumisión.'} icon={<BoxingGlovesIcon style={{fontSize: 30}} />} image={'jujitsu_ai'} title={'Jujitsu'} />
+                        <HoveringImage description={'Deporte de combate con golpes y patadas.'} icon={<BoxingGlovesIcon style={{fontSize: 30}} />} image={'kickboxing_ai'} title={'Kickboxing'} />
+                        <HoveringImage description={'Arte marcial tailandés de ocho extremidades.'} icon={<BoxingGlovesIcon style={{fontSize: 30}} />} image={'muaythai_ai'} title={'Muay Thai'} />
+                        <HoveringImage description={'Lucha cuerpo a cuerpo en busca de dominación.'} icon={<BoxingGlovesIcon style={{fontSize: 30}} />} image={'olimpic_ai'} title={'Lucha olímpica'} />
+                        <HoveringImage description={'Deporte de combate mixto con múltiples disciplinas.'} icon={<BoxingGlovesIcon style={{fontSize: 30}} />} image={'mma_ai'} title={'MMA'} />
+                        <ZoneTitle
+                            lineup={'Infinite Kids'}
+                            title={'Crecimiento y diversión'}
+                            description={'Para los mas pequeños de la casa, tenemos un programa de entrenamiento diseñado para que se diviertan y desarrollen habilidades físicas y mentales.'}
+                        />
+                        <HoveringImage description={'Arte marcial de defensa personal.'} icon={<BoxingGlovesIcon style={{fontSize: 30}} />} image={'kids1'} title={'Jujitsu Kids'} />
+                        <HoveringImage description={'Deporte de combate con golpes y patadas.'} icon={<BoxingGlovesIcon style={{fontSize: 30}} />} image={'kids2'} title={'Kickboxing Kids'} />
+                        <HoveringImage description={'Campamento de verano de artes marciales.'} icon={<BoxingGlovesIcon style={{fontSize: 30}} />} image={'kids3'} title={'Campamento Vacacional'} />
                     </Row>
                 </div>
             </div>
